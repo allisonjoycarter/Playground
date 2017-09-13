6c2a49cc1fb31e65871259debfb0f96e93646468
terrainbow <31906811+terrainbow@users.noreply.github.com>
2017-09-12 20:41:33 -0400

    Add files via upload

---
+++ Playground/Menu.java
@@ -0,0 +1,41 @@
+package Playground;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu {
+    private ArrayList<String> options = new ArrayList<>();
+
+    public Menu() {
+
+    }
+
+    public void addOption(){//add an option to the menu
+        options.add("Option 1");
+        options.add("Option 2");
+        options.add("Option 3");
+
+        System.out.println(options);
+
+    }
+    public void delOption(){//remove an option from the menu
+        options.remove(2);
+
+        System.out.println("Options after removal: " + options);
+
+
+    }
+    public void runMenu(){//makes a menu in the UI
+
+    }
+    public void changeOption(){//CHALLENGE: change an option from one thing to another
+
+    }
+
+
+    //hint: who says menu has to be the only object you create?
+
+
+
+
+}
